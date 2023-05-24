bb70378422129c28aa74281542a5e70be30b68b4
enyo90 <rebeccabrilly@gmail.com>
2023-05-23 17:18:18 -0700

    poppies

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,28 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: array
- * @name: name
- * @age: age
- * @owner: owner
- * Description: This struct is for dog
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
  */
-
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+		printf("Name: %s\n", d->name);
 
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
